aa04f577d1ff69ac5deeac3650c621018064966b
Eric Oliveira <oiericoi@hotmail.co>
2023-06-11 15:50:17 -0300

    test: modify ola_test file

--- ola_test.go
+++ ola_test.go
@@ -10,17 +10,31 @@ func TestOla(t *testing.T) {
 			t.Errorf("result: '%s', expected: '%s'", result, expected)
 		}
 	}
-	
+
 	t.Run("should say hello to people", func(t *testing.T) {
-		result := Ola("Eric")
+		result := Hello("Eric", "")
 		expected := "Olá, Eric"
 	
 		checkCorrectMessage(t, result, expected)
 	})
 
 	t.Run("should say 'Hello world' when an empty string is passed", func(t *testing.T) {
-		result := Ola("")
+		result := Hello("", "")
 		expected := "Olá, Mundo"
+
+		checkCorrectMessage(t, result, expected)
+	})
+
+	t.Run("should say hello in Spanish", func(t *testing.T) {
+		result := Hello("Eric", "spanish")
+		expected := "Hola, Eric"
+
+		checkCorrectMessage(t, result, expected)
+	})
+
+	t.Run("should say hello in French", func(t *testing.T) {
+		result := Hello("Eric", "french")
+		expected := "Bonjour, Eric"
 
 		checkCorrectMessage(t, result, expected)
 	})
