f7503bcc536044991b617b44d674082d37d2f86b
Eric Oliveira <oiericoi@hotmail.co>
2023-06-11 13:14:57 -0300

    refactor: improving tests

--- ola_test.go
+++ ola_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	result := Ola("Eric")
-	expected := "Olá, Eric"
+	checkCorrectMessage := func (t *testing.T, result, expected string)  {
+		t.Helper()
 
-	if result != expected {
-		t.Errorf("result: '%s', expected: '%s'", result, expected)
+		if result != expected {
+			t.Errorf("result: '%s', expected: '%s'", result, expected)
+		}
 	}
+	
+	t.Run("should say hello to people", func(t *testing.T) {
+		result := Ola("Eric")
+		expected := "Olá, Eric"
+	
+		checkCorrectMessage(t, result, expected)
+	})
+
+	t.Run("should say 'Hello world' when an empty string is passed", func(t *testing.T) {
+		result := Ola("")
+		expected := "Olá, Mundo"
+
+		checkCorrectMessage(t, result, expected)
+	})
 }
