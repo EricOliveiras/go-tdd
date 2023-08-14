9ca28f6851dc625e5b0bb942a4e87861e382fd3d
Eric Oliveira <oiericoi@hotmail.com>
2023-08-14 10:19:05 -0300

    refactor: modify ola.go

--- ola/ola.go
+++ ola/ola.go
@@ -3,15 +3,29 @@ package main
 import "fmt"
 
 const prefixoOlaPortugues = "Olá, "
+const prefixoOlaEspanhol = "Hola, "
+const prefixoOlaFrances = "Bonjour, "
 
-func Ola(nome string) string {
+func Ola(nome, idioma string) string {
 	if nome == "" {
 		nome = "Mundo"
 	}
-	return prefixoOlaPortugues + nome
+
+	return prefixoSaudacao(idioma) + nome
+}
+
+func prefixoSaudacao(idioma string) (prefixo string) {
+	switch idioma {
+	case "espanhol":
+		prefixo = prefixoOlaEspanhol
+	case "frances":
+		prefixo = prefixoOlaFrances
+	default:
+		prefixo = prefixoOlaPortugues
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Ola("mundo"))
+	fmt.Println(Ola("mundo", "espanhol"))
 }
-
