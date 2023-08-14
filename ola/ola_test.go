56a59e6b8cd9ec13f90a50be8b38df390331f80b
Eric Oliveira <oiericoi@hotmail.com>
2023-08-14 10:18:47 -0300

    refactor: modify ola test

--- ola/ola_test.go
+++ ola/ola_test.go
@@ -11,17 +11,32 @@ func TestOla(t *testing.T) {
 			t.Errorf("resultado: %s - esperado: %s", resultado, esperado)
 		}
 	}
+
 	t.Run("diz olá para as pessoas", func(t *testing.T) {
-		resultado := Ola("Eric")
+		resultado := Ola("Eric", "")
 		esperado := "Olá, Eric"
 
 		verificaMensagemCorreta(t, resultado, esperado)
 	})
 
 	t.Run("diz 'Olá, mundo' qunado uma string vazia for passada", func(t *testing.T) {
-		resultado := Ola("")
+		resultado := Ola("", "")
 		esperado := "Olá, Mundo"
 
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("diz olá em espanhol", func(t *testing.T) {
+		resultado := Ola("Eric", "espanhol")
+		esperado := "Hola, Eric"
+
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("diz olá em francês", func(t *testing.T) {
+		resultado := Ola("Eric", "frances")
+		esperado := "Bonjour, Eric"
+		
 		verificaMensagemCorreta(t, resultado, esperado)
 	})
 }
